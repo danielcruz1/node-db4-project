40e2d9812a3db75647ea6bcf43cd4364916e39d7
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-06 20:24:07 -0700

    migration and seed cleanup files added

--- data/migrations/20200206194003_recipe-book.js
+++ data/migrations/20200206194003_recipe-book.js
@@ -1,8 +1,32 @@
 
 exports.up = function(knex) {
-  
+  return (knex.schema
+    .createTable('recipes', recipe => {
+        recipe.increments();
+        recipe.string('name', 128).notNullable();
+    }))
+    .createTable('steps', step => {
+        step.increments();
+        step.integer('step_number').unsigned().references('recipe_id');
+        step.text('instructions', 256);
+    })
+    .createTable('ingredients', ingredient => {
+        ingredient.increments();
+        ingredient.string('name', 128);
+    })
+    .createTable('combine_recipe_ingredients', combined => {
+        combined.increments();
+        combined.integer('recipe_id').notNullable().unsigned().references('recipe_id');
+        combined.integer('ingredients_id').notNullable().unsigned().references('ingredients_id');
+        combined.float('quantity');
+    })
 };
 
 exports.down = function(knex) {
-  
+  return (knex.schema
+    .dropTableIfExists('combine_recipe_ingredients')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('recipes')    
+    )
 };
